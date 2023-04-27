87933c7bb8eae0c38c379bfbd6926f9ab6fde712
Ilir <ilir.hajdari111@gmail.com>
2023-04-26 20:55:10 -0400

    pushing the api changes made to html and js files

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic3dteXRob3MiLCJhIjoiY2xndms2eWphMmpoYTNqbWticjVlcng1NyJ9.6Qgtg_xsE15r4ab7V1p2qg";
-const map = new mapboxgl.Map({
-  container: "mapContainer", // HTML container ID
+
+var map = new mapboxgl.Map({
+  container: "mapContainer",
   style: "mapbox://styles/mapbox/streets-v12",
-  center: [],
-  zoom: 2.5,
+  center: [0, 0],
+  zoom: 2,
 });
